f91c716b8e91aa3f5bfbffdafd24816b48fc68e9
Rashita Mehta <56469693+Rashita123@users.noreply.github.com>
2021-02-20 02:16:42 +0530

    feat: Background and Text Color feat added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { useState } from "react";
 export default function App() {
   const [fontsize, setFontSize] = useState(20);
   const [fontFamily, setFontFamily] = useState("Arial");
+  const [bgColor, setBgColor] = useState("#FFFFFF");
+  const [textColor, setTextColor] = useState("#000000");
 
   const availableFontFamily = ["Arial", "cursive", "Times New Roman"];
 
@@ -14,12 +16,18 @@ export default function App() {
     if (fontsize > 15) setFontSize(fontsize - 8);
   }
   function giveFontSize() {
-    return { fontSize: fontsize + "px", fontFamily: fontFamily };
+    return {
+      fontSize: fontsize + "px",
+      fontFamily: fontFamily,
+      background: bgColor,
+      borderRadius: "3px",
+      color: textColor
+    };
   }
 
   function SelectFontSizeButtons() {
     return (
-      <div style={{ display: "inline", margin: "2rem" }}>
+      <div style={{ display: "inline", margin: " 1rem 2rem" }}>
         <button onClick={increaseFontSizeHandler}>+</button>
         <span>{fontsize}</span>
         <button onClick={decreaseFontSizeHandler}>-</button>
@@ -36,7 +44,7 @@ export default function App() {
       <select
         selected
         value={fontFamily}
-        style={{ display: "inline", margin: "2rem" }}
+        style={{ display: "inline", margin: " 1rem 2rem" }}
         onChange={checkvalue}
       >
         {availableFontFamily.map((font) => (
@@ -45,21 +53,78 @@ export default function App() {
       </select>
     );
   }
+  function textColorHandler(event) {
+    setTextColor(event.target.value);
+  }
+
+  function bgColorHandler(event) {
+    setBgColor(event.target.value);
+  }
+
+  function SelectBackgroundColor() {
+    return (
+      <input
+        style={{ display: "inline", margin: " 1rem 2rem" }}
+        onChange={bgColorHandler}
+        type="color"
+        value={bgColor}
+      />
+    );
+  }
+
+  function SelectTextColor() {
+    return (
+      <input
+        style={{ display: "inline", margin: " 1rem 2rem" }}
+        onChange={textColorHandler}
+        type="color"
+        value={textColor}
+      />
+    );
+  }
+
   return (
     <div className="App">
       <h1>Mini Figma</h1>
+
+      {/* //FontSize Section start */}
+
       <h3 style={{ display: "inline" }}>
         <u>Set Font Size</u>
       </h3>
       <SelectFontSizeButtons />
+
       <br></br>
+
+      {/* Font Family Section Start */}
+
       <h3 style={{ display: "inline" }}>
         <u>Set Font Size</u>
       </h3>
       <SelectFontFamily />
 
+      <br></br>
+
+      {/* Set Background Section Start */}
+
+      <h3 style={{ display: "inline" }}>
+        <u>Set Background Color</u>
+      </h3>
+
+      <SelectBackgroundColor />
+
+      <br></br>
+
+      <h3 style={{ display: "inline" }}>
+        <u>Set Text Color</u>
+      </h3>
+      <SelectTextColor />
+
+      {/* Text Display Section Start */}
+
       <p style={giveFontSize()}>Font will be applied to this text</p>
 
+      {/* Css copy section start */}
       <div className="copyCSSstyling">
         <h3>
           <u>Copy CSS from here</u>
@@ -67,8 +132,12 @@ export default function App() {
         <div style={{ width: "30%", margin: "auto" }}>
           <p>font-size: {fontsize}px;</p>
           <p>font-family: {fontFamily}</p>
+          <p>background-color: {bgColor}</p>
+          <p>color: {textColor}</p>
         </div>
       </div>
+
+      {/* All Done */}
     </div>
   );
 }
